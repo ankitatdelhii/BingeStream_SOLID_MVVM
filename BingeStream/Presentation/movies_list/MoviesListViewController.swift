f45022910e23917dcbdc3453f06b1ecbda0e0297
Ankit Saxena <ankitatdelhii@gmail.com>
2023-04-14 23:45:52 +0530

    presentation layer added

---
+++ BingeStream/Presentation/movies_list/MoviesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesListViewController.swift
+//  BingeStream
+//
+//  Created by Ankit Saxena on 14/04/23.
+//
+
+import UIKit
+
+class MoviesListViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ BingeStream/Presentation/movies_list/MoviesListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesListViewModel.swift
+//  BingeStream
+//
+//  Created by Ankit Saxena on 14/04/23.
+//
+
+import Foundation
+
+class MoviesListViewModel {
+    
+}

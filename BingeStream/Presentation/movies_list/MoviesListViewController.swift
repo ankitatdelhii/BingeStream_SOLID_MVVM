063816fa270505507480e81216a9e77e84076df2
Ankit Saxena <ankitatdelhii@gmail.com>
2023-04-18 03:11:02 +0530

    presentation layer wip

--- BingeStream/Presentation/movies_list/MoviesListViewController.swift
+++ BingeStream/Presentation/movies_list/MoviesListViewController.swift
@@ -12,13 +12,28 @@ class MoviesListViewController: UIViewController {
     //MARK: Outelets
     
     //MARK: Properties
-    private var viewmodel: MoviesListModel!
+    private var viewmodel: MoviesListViewModel!
     
     //MARK: UI Properties
     
     //MARK: Lifecycle
+    
+    class func instance() -> MoviesListViewController {
+        
+        let controller = MoviesListViewController.init(nibName: "MoviesListViewController", bundle: nil)
+        
+        let localDataCordinator = LocalDataCoordinatorImpl(localDataStorage: UserDefaultsLocalDataStorage())
+        let nasaImagesListRepo = NasaImagesListRepositoryImpl(localDataCordinator: localDataCordinator, networkClient: NetworkClient(dispatcher: URLSessionNetworkDispatcher()))
+        let nasaImagesListUseCase = NasaImagesListUseCaseImpl(nasaImagesListRepository: nasaImagesListRepo)
+        let viewmodel = MoviesListViewModel(nasaImagesListUseCase: nasaImagesListUseCase)
+        
+        controller.viewmodel = viewmodel
+        return controller
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewmodel.executeUseCase()
     }
     
     //MARK: Helpers
--- BingeStream/Presentation/movies_list/MoviesListViewModel.swift
+++ BingeStream/Presentation/movies_list/MoviesListViewModel.swift
@@ -9,4 +9,32 @@ import Foundation
 
 class MoviesListViewModel {
     
+    let nasaImagesListUseCase: NasaImagesListUseCase
+    
+    init(nasaImagesListUseCase: NasaImagesListUseCase) {
+        self.nasaImagesListUseCase = nasaImagesListUseCase
+    }
+    
+    func executeUseCase() {
+        nasaImagesListUseCase.execute { cachedResult in
+            switch cachedResult {
+                
+            case .success(let cachedData):
+                print("Got Cached Data \(cachedData)")
+            case .failure(let failure):
+                print("Got Cached Data failure \(failure)")
+            }
+        } apiImages: { apiResult in
+            switch apiResult {
+                
+            case .success(let apiData):
+                print("Got Api Data \(apiData)")
+            case .failure(let failure):
+                print("Got Api Data failure \(failure)")
+            }
+        }
+
+    }
+    
+    
 }
